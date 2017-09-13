b64169a95d0c83edb02415498664dc750f903b2b
hagenwatkins <hd_watkins@yahoo.com>
2017-09-13 15:31:09 -0500

    updated

--- src/main/java/com/locafella/tmbd/Controlla.java
+++ src/main/java/com/locafella/tmbd/Controlla.java
@@ -9,31 +9,43 @@ import java.lang.reflect.Array;
 import java.util.Collections;
 import java.util.List;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Controller
 public class Controlla {
     @RequestMapping(path = "/")
-    public String homePage(){
+    public String homePage() {
         return "home";
     }
 
     @RequestMapping(path = "/now-playing")
     public String nowPlaying(Model model) {
-    model.addAttribute("movies", getMovies("now-playing"));
+        model.addAttribute("movies", getMovies("now-playing"));
+
 
         return "now-playing";
     }
+
     @RequestMapping(path = "/medium-popular-long-name")
-        public String mediumPage() {
+    public String mediumPage(Model model) {
+
+        List<Movie> filteredMovies = getMovies("now-playing")
+                .stream()
+                .filter(movie -> movie.getPopularity() >= 30 && movie.getPopularity() <= 80 && movie.getTitle().length() >= 10)
+                .collect(Collectors.toList());
+        model.addAttribute("movies", filteredMovies);
         return "medium-popular-long-name";
+
     }
-    public static List<Movie> getMovies(String route){
+
+
+    public static List<Movie> getMovies(String route) {
         RestTemplate restTemplate = new RestTemplate();
-        ResultsPage movies = restTemplate.getForObject("https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb",ResultsPage.class);
+        ResultsPage movies = restTemplate.getForObject("https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb", ResultsPage.class);
 
         return movies.results;
 
     }
-
-    public static List<Movie>
 }
+
+
